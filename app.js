a6580b14b7c0b1c4af5873a870628eca6cf75799
cesaraugusto316 <cesaraugusto316@protonmail.com>
2022-07-09 23:18:27 -0500

    my first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5_000; // prod | dev
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      message: "hello world aliebnb",
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`[Server⚡] running on port: ${PORT}`);
+});
